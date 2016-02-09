23e3d3875eeb83cb9491d6043316f3bc9c61de39
maikerumine <michaellorenger@yahoo.com>
2016-02-08 21:19:06 -0500

    full on mobs

--- mods/esmobs/init.lua
+++ mods/esmobs/init.lua
@@ -23,24 +23,126 @@ esmobs={}
 	--BECAUSE I SUCK AT PROGRAMMING THIS IS THE OVER RIDE TO THE BELOW CODE
 		dofile(minetest.get_modpath("esmobs").."/esanimal.lua")
 		dofile(minetest.get_modpath("esmobs").."/esnpc.lua")
-		--dofile(minetest.get_modpath("esmobs").."/mtmonster.lua")
-		--dofile(minetest.get_modpath("esmobs").."/mcmonster.lua")
+		dofile(minetest.get_modpath("esmobs").."/mtmonster.lua")
 		dofile(minetest.get_modpath("esmobs").."/esbadplayer.lua")
 		dofile(minetest.get_modpath("esmobs").."/esmonster.lua")
-		--dofile(minetest.get_modpath("esmobs").."/esnpc2.lua")
-		--dofile(minetest.get_modpath("esmobs").."/esbadplayer2.lua")
+		dofile(minetest.get_modpath("esmobs").."/esnpc2.lua")
+		dofile(minetest.get_modpath("esmobs").."/esbadplayer2.lua")
 
+--[[
+--MOB SETTINGS leave this alone, use config file to change.
+	if (esmobs.MOB_SETTING ==  1) then   --Just Animals.
+		dofile(minetest.get_modpath("esmobs").."/esanimal.lua")
+	end
+
+	if (esmobs.MOB_SETTING == 2) then	--Animals and good npc's.
+		dofile(minetest.get_modpath("esmobs").."/esanimal.lua")
+		dofile(minetest.get_modpath("esmobs").."/esnpc.lua")
+		if es then
+			dofile(minetest.get_modpath("esmobs").."/esnpc2.lua")
+		end
+	end
+
+	if (esmobs.MOB_SETTING == 3) then	--Animals, Good npc, and MT monsters.
+		dofile(minetest.get_modpath("esmobs").."/esanimal.lua")
+		dofile(minetest.get_modpath("esmobs").."/esnpc.lua")
+		dofile(minetest.get_modpath("esmobs").."/mtmonster.lua")
+		if es then
+			dofile(minetest.get_modpath("esmobs").."/esnpc2.lua")
+		end
+	end
+
+	if (esmobs.MOB_SETTING == 4) then	--Animals, Good npc, and, MT monsters.
+		dofile(minetest.get_modpath("esmobs").."/esanimal.lua")
+		dofile(minetest.get_modpath("esmobs").."/esnpc.lua")
+		dofile(minetest.get_modpath("esmobs").."/mtmonster.lua")
+		if es then
+			dofile(minetest.get_modpath("esmobs").."/esnpc2.lua")
+		end
+	end
+
+	if (esmobs.MOB_SETTING == 5) then	--Animals, Good npc, MT monsters, ES monsters,  and Bad npc's.   All 76 of them.
+		dofile(minetest.get_modpath("esmobs").."/esanimal.lua")
+		dofile(minetest.get_modpath("esmobs").."/esnpc.lua")
+		dofile(minetest.get_modpath("esmobs").."/mtmonster.lua")
+		dofile(minetest.get_modpath("esmobs").."/esbadplayer.lua")
+		dofile(minetest.get_modpath("esmobs").."/esmonster.lua")
+		if es then
+			dofile(minetest.get_modpath("esmobs").."/esnpc2.lua")
+			dofile(minetest.get_modpath("esmobs").."/esbadplayer2.lua")
+		end
+	end
+
+	if (esmobs.MOB_SETTING == 6) then	--MT Monsters, Bad npc's.
+		dofile(minetest.get_modpath("esmobs").."/mtmonster.lua")
+		dofile(minetest.get_modpath("esmobs").."/esbadplayer.lua")
+		if es then
+			dofile(minetest.get_modpath("esmobs").."/esbadplayer2.lua")
+		end
+	end
+
+	if (esmobs.MOB_SETTING == 7) then	--Good npc, Bad npc's.
+		dofile(minetest.get_modpath("esmobs").."/esnpc.lua")
+		dofile(minetest.get_modpath("esmobs").."/esbadplayer.lua")
+		if es then
+			dofile(minetest.get_modpath("esmobs").."/esnpc2.lua")
+			dofile(minetest.get_modpath("esmobs").."/esbadplayer2.lua")
+		end
+	end
+
+	if (esmobs.MOB_SETTING == 8) then	 Animals.
+		dofile(minetest.get_modpath("esmobs").."/esanimal.lua")
+	end
+
+	if (esmobs.MOB_SETTING == 9) then	--MT monsters and Animals.
+		dofile(minetest.get_modpath("esmobs").."/esanimal.lua")
+		dofile(minetest.get_modpath("esmobs").."/mtmonster.lua")
+	end
+
+	if (esmobs.MOB_SETTING == 10) then	--Good npc's.
+		dofile(minetest.get_modpath("esmobs").."/esnpc.lua")
+		if es then
+			dofile(minetest.get_modpath("esmobs").."/esnpc2.lua")
+		end
+	end
+
+	if (esmobs.MOB_SETTING == 11) then	--MT monsters, ES monsters, and Bad npc's.
+		dofile(minetest.get_modpath("esmobs").."/mtmonster.lua")
+		dofile(minetest.get_modpath("esmobs").."/esbadplayer.lua")
+		dofile(minetest.get_modpath("esmobs").."/esmonster.lua")
+		if es then
+			dofile(minetest.get_modpath("esmobs").."/esbadplayer2.lua")
+		end
+	end
+
+	if (esmobs.MOB_SETTING == 12) then	--MT Monsters.
+		dofile(minetest.get_modpath("esmobs").."/mtmonster.lua")
+	end
+]]
+--MOBS
+	--dofile(minetest.get_modpath("esmobs").."/esnpc.lua")
+	--dofile(minetest.get_modpath("esmobs").."/esmonster.lua")
+	--dofile(minetest.get_modpath("esmobs").."/mtmonster.lua")
+	--dofile(minetest.get_modpath("esmobs").."/esbadplayer.lua")
+	--dofile(minetest.get_modpath("esmobs").."/esbadplayer2.lua")
+	--dofile(minetest.get_modpath("esmobs").."/esanimal.lua")
 
 --CRAFTS AND MISC
 	dofile(minetest.get_modpath("esmobs").."/crafts.lua")
-	--dofile(minetest.get_modpath("esmobs").."/spawner.lua")
+	dofile(minetest.get_modpath("esmobs").."/spawner.lua")
 
 --MOB SEPCS
 	if inventory_plus then
 		dofile(minetest.get_modpath("esmobs").."/mobspec.lua")
 	end
 
-
+--IF ES IS LOADED YOU WILL SEE OTHER MOBS HOLDING THE SPECIAL TOOLS
+	--if es then
+		--dofile(minetest.get_modpath("esmobs").."/esnpc2.lua")
+	--end
+	--if es then
+		--dofile(minetest.get_modpath("esmobs").."/esbadplayer2.lua")
+	--end
 
 --IN CASE BONES IS NOT INSTALLED, THIS OVERRIDES NODES SO YOU HAVE THEM FOR MOBS.
 	if not bones then
